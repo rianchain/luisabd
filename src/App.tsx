222fb03eab3343b96556bae75f2611476072bece
etherian <rianziwalker@gmail.com>
2024-12-07 12:09:47 +0700

    photoSlide func disable

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Hero } from './components/Hero';
-import { Slideshow } from './components/Slideshow';
-import { Message } from './components/Message';
-import { PhotoGallery } from './components/PhotoGallery';
+import React from "react";
+import { Hero } from "./components/Hero";
+import { Slideshow } from "./components/Slideshow";
+import { Message } from "./components/Message";
+import { PhotoGallery } from "./components/PhotoGallery";
 
 function App() {
   return (
     <div className="min-h-screen">
       <Hero />
       <Message />
-      <Slideshow />
+      {/* <Slideshow /> */}
       <PhotoGallery />
     </div>
   );
