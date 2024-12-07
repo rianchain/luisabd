81fb4d73d737c1ea053df1cb1a06f817aa7e43f2
etherian <rianziwalker@gmail.com>
2024-12-07 21:06:43 +0700

    added a new components and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Hero } from "./components/Hero";
 import { Slideshow } from "./components/Slideshow";
 import { Message } from "./components/Message";
 import { PhotoGallery } from "./components/PhotoGallery";
+import Heart from "./components/Heart";
 
 function App() {
+  const [position, setPosition] = useState<{ x: Number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+
   return (
     <div className="min-h-screen">
       <Hero />
